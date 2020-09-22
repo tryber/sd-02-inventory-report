895a4d571b85bf700ca9adf1b50b8f3df04b7b54
guicgs <etohlif@protonmail.com>
2020-09-22 18:02:48 -0300

    requisito 1 concluído

--- reports/simple_report.py
+++ reports/simple_report.py
@@ -1,41 +1,62 @@
-import datetime
+from datetime import datetime
+from operator import itemgetter
+from collections import Counter
 
 
 class SimpleReport:
-    def __init__(self):
-        self.__oldest_fabrication = ""
-        self.__closest_valid_date = ""
-        self.__most_products_employee_name = ""
+    @classmethod
+    def generate(cls, products):
+        oldest_fabrication = min(
+            products, key=itemgetter("data_de_fabricacao")
+        )["data_de_fabricacao"]
+        nearest_shelf_life = min(
+            [
+                prod
+                for prod in products
+                if prod["data_de_validade"]
+                >= datetime.now().strftime("%Y-%m-%d")
+            ],
+            key=itemgetter("data_de_validade"),
+        )["data_de_validade"]
+        most_products_company_name = max(
+            Counter(prod["nome_da_empresa"] for prod in products)
+        )
+        print(f"Data de fabricação mais antiga: {oldest_fabrication}")
+        print(f"Data de validade mais próxima: {nearest_shelf_life}")
+        print(
+            f"Empresa com maior quantidade de produtos estocados: {most_products_company_name}"
+        )
 
-    def get_dates(products, key):
-        return min(products, key=lambda x: x[key])
 
-    
-    def generate(products):
-        
-
-
-prod = [
-  {
-    "id": 1,
-    "nome_do_produto": "Uai",
-    "nome_da_empresa": "Forces of Nature",
-    "data_de_fabricacao": "2024-06-02",
-    "data_de_validade": "2023-02-20",
-    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
-  },
-  {
-    "id": 2,
-    "nome_do_produto": "Sô",
-    "nome_da_empresa": "Forces of Nature",
-    "data_de_fabricacao": "2021-07-28",
-    "data_de_validade": "2023-02-21",
-    "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
-    "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices    phasellus"
-  }
+products = [
+    {
+        "id": 1,
+        "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP, GERANIUM MACULATUM ROOT, SODIUM CHLORIDE, THUJA OCCIDENTALIS LEAFY TWIG, ZINC, and ECHINACEA ANGUSTIFOLIA",
+        "nome_da_empresa": "Forces of Nature",
+        "data_de_fabricacao": "2024-06-02",
+        "data_de_validade": "2023-01-01",
+        "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+        "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus",
+    },
+    {
+        "id": 2,
+        "nome_do_produto": "Sô",
+        "nome_da_empresa": "Forces of Nature",
+        "data_de_fabricacao": "2021-07-28",
+        "data_de_validade": "2021-01-10",
+        "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+        "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus",
+    },
+    {
+        "id": 3,
+        "nome_do_produto": "Uai Sô",
+        "nome_da_empresa": "Folgado Inc.",
+        "data_de_fabricacao": "2021-07-22",
+        "data_de_validade": "2021-01-11",
+        "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+        "instrucoes_de_armazenamento": "in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus",
+    },
 ]
 
 
-SimpleReport.get_dates(prod, "data_de_validade")
-SimpleReport.get_dates(prod, "data_de_fabricacao")
+SimpleReport.generate(products)
