b3834b5ec65371728849920bb34fc6c0a65e39cf
lauro <laurolyra@gmail.com>
2020-09-22 18:44:06 -0300

    loop ok

--- reports/simple_report.py
+++ reports/simple_report.py
@@ -1,5 +1,7 @@
 import json
+import math
 from datetime import datetime, timedelta, date
+from collections import Counter
 
 with open('../data/inventory_20200823.json') as json_file:
     data = json.load(json_file)
@@ -14,16 +16,20 @@ class SimpleReport:
 
 production = (sorted(data, key = lambda i: i['data_de_fabricacao']))
 expiration = (sorted(data, key = lambda i: i['data_de_validade']))
-today = date.today()
-print("Today's date:", today)
-
+expiration_date = None
+days_diff = math.inf
+most_products = Counter(empresa["nome_da_empresa"] for empresa in data).most_common(1)[0][0]
+print(most_products)
+# percorrer todos os 
 for index, industry in enumerate(data):
     today = datetime.strptime(str(date.today()), '%Y-%m-%d')
     converted_exp = datetime.strptime(industry['data_de_validade'], '%Y-%m-%d')
     date_diff = abs((today - converted_exp).days)
-    print(date_diff)
-    
-    # print("Data de fabricação mais antiga: YYYY-MM-DD")
-    # print("Data de validade mais próxima: YYYY-MM-DD")
-    # print("Empresa com maior quantidade de produtos estocados: NOME DA EMPRESA")
+    if (date_diff < days_diff):
+        days_diff = date_diff
+        expiration_date = industry['data_de_validade']
+
+print(f'Data de fabricação mais antiga: {production[0]["data_de_fabricacao"]}')
+print(f'Data de validade mais próxima: {expiration_date}')
+print(f'Empresa com maior quantidade de produtos estocados: {most_products}')
 
