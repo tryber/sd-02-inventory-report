7670acc86c929b487828e84a98c3201f3d89bc42
phtf92 <phtf92@gmail.com>
2020-09-23 16:00:51 -0300

    Req 3

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -1,12 +1,28 @@
 from reports.simple_report import SimpleReport
 from reports.complete_report import CompleteReport
+import os.path
+import csv
 
 
 class Inventory():
     @classmethod
-    def import_data(cls, type, arquive):
-        if (type == 'simple'):
-            return SimpleReport.generate(arquive)
-        if (type == 'complete'):
-            return CompleteReport.generate(arquive)
+    def import_data(cls, report_type, arquive):
+        extension = os.path.splitext(arquive)[1]
+        type_file = {
+                    ".json": 'JSON',
+                    ".csv": cls.convert_csv_to_Dict(arquive),
+                    ".xml": 'xml'
+                    }
+        file_to_report = type_file[extension]
 
+        if (report_type == 'simple'):
+            return SimpleReport.generate(file_to_report)
+        if (report_type == 'complete'):
+            return CompleteReport.generate(file_to_report)
+
+    @classmethod
+    def convert_csv_to_Dict(cls, path_to_file):
+        with open(path_to_file) as file:
+            header, *csvLines = csv.reader(file, delimiter=",")
+            return [{item: line[index] for index, item in enumerate(header)}
+                    for line in csvLines]
