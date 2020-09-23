e181b0dab13ea075c5da3e6d6766eecb1e247e61
phtf92 <phtf92@gmail.com>
2020-09-23 17:08:08 -0300

    Req 5

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -3,32 +3,45 @@ from reports.complete_report import CompleteReport
 import os.path
 import csv
 import json
+from xml.dom import minidom
 
 
-class Inventory():
+class Inventory:
     @classmethod
     def import_data(cls, report_type, arquive):
         extension = os.path.splitext(arquive)[1]
-        type_file = {
-                    ".json": cls.convert_json_to_Dict(arquive),
-                    ".csv": cls.convert_csv_to_Dict(arquive),
-                    ".xml": 'xml'
-                    }
-        file_to_report = type_file[extension]
 
-        if (report_type == 'simple'):
+        type_file = {
+            ".json": cls.convert_json_to_Dict,
+            ".csv": cls.convert_csv_to_Dict,
+            ".xml": cls.convert_xml_to_Dict,
+        }
+        file_to_report = type_file[extension](arquive)
+
+        if report_type == "simple":
             return SimpleReport.generate(file_to_report)
-        if (report_type == 'complete'):
+        if report_type == "complete":
             return CompleteReport.generate(file_to_report)
 
     @classmethod
     def convert_csv_to_Dict(cls, path_to_file):
         with open(path_to_file) as file:
             header, *csvLines = csv.reader(file, delimiter=",")
-            return [{item: line[index] for index, item in enumerate(header)}
-                    for line in csvLines]
+            return [
+                {item: line[index] for index, item in enumerate(header)}
+                for line in csvLines
+            ]
 
     @classmethod
     def convert_json_to_Dict(cls, path_to_file):
         with open(path_to_file) as file:
             return json.load(file)
+
+    @classmethod
+    def convert_xml_to_Dict(cls, path_to_file):
+        xml = minidom.parse(path_to_file)
+        records = xml.getElementsByTagName("record")
+        return [
+            {node.tagName: node.firstChild.data for node in record.childNodes}
+            for record in records
+        ]
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from inventory.inventory import Inventory
 
 
-print(Inventory.import_data('complete', 'data/inventory_20200823.json'))
+print(Inventory.import_data('simple', 'data/inventory_20200823.xml'))
+
+#print(Inventory.convert_xml_to_Dict('data/inventory_20200823.xml'))
