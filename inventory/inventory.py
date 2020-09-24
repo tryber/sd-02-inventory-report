2a0653d7401cb90acb98e19a1676945e27ca5148
richardwellerson <richardwellerson@gmail.com>
2020-09-24 17:20:37 -0300

    Refatorando

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -1,11 +1,13 @@
 from reports.simple_report import SimpleReport
 from reports.complete_report import CompleteReport
-from importer.csv_importer import CsvImporter
-from importer.json_importer import JsonImporter
-from importer.xml_importer import XmlImporter
 
 
 class Inventory:
+    def __init__(self, importer):
+        self.data = []
+        self.importer = importer
+
+
     @classmethod
     def generate_report(cls, file, request_type):
         if request_type == "simples":
@@ -13,17 +15,6 @@ class Inventory:
         if request_type == "completo":
             return CompleteReport.generate(file)
 
-    @classmethod
-    def import_data(cls, file, request_type):
-        if file.endswith(".csv"):
-            return cls.generate_report(
-                CsvImporter.import_data(file), request_type
-            )
-        if file.endswith(".json"):
-            return cls.generate_report(
-                JsonImporter.import_data(file), request_type
-            )
-        if file.endswith(".xml"):
-            return cls.generate_report(
-                XmlImporter.import_data(file), request_type
-            )
+    def import_data(self, file, request_type):
+        self.data = self.importer.import_data(file)
+        return self.generate_report(self.data, request_type)
