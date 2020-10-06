2586cd40015650874807b3277b163613954ab2e7
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-10-06 14:07:06 -0300

    Correção para a AV2

--- reports/simple_report.py
+++ reports/simple_report.py
@@ -1,6 +1,5 @@
 import math
-from datetime import datetime, date
-from operator import itemgetter
+from datetime import datetime
 from collections import Counter
 
 
@@ -9,39 +8,25 @@ class SimpleReport:
     def generate(cls, dict_file):
         production = (sorted(dict_file, key=lambda i: i['data_de_fabricacao']))
         expiration_date = None
-        days_diff = math.inf
+        days_diff = -math.inf
 
         most_products = Counter(
             industry["nome_da_empresa"] for industry in dict_file
         ).most_common(1)[0][0]
 
         for index, industry in enumerate(dict_file):
-            today = datetime.now().strftime("%Y-%m-%d")
-            if industry["data_de_validade"] > today:
-                expiration_date_test = industry['data_de_validade']
+            today = datetime.now()
+            converted_exp = datetime.strptime(
+                industry['data_de_validade'], '%Y-%m-%d')
+            date_diff = (today - converted_exp).days
 
-        # 34-39 needs python3.8 (results in 2024-08-16)
-        list_date_val = [
-            date.fromisoformat(industry["data_de_validade"])
-            for item in dict_file
-            if date.fromisoformat(industry["data_de_validade"]) >= date.today()
-        ]
-        list_date_val.sort()
-
-        # 42-50 results in 2022-08-23
-        # for index, industry in enumerate(dict_file):
-        #     today = datetime.strptime(str(date.today()), '%Y-%m-%d')
-        #     converted_exp = datetime.strptime(
-        #         industry['data_de_validade'], '%Y-%m-%d')
-        #     date_diff = abs((today - converted_exp).days)
-
-        #     if (date_diff < days_diff):
-        #         days_diff = date_diff
-        #         expiration_date = industry['data_de_validade']
+            if (date_diff > days_diff and date_diff <= 0):
+                days_diff = date_diff
+                expiration_date = industry['data_de_validade']
 
         return(
             f'Data de fabricação mais antiga:'
             f' {production[0]["data_de_fabricacao"]}\n'
-            f'Data de validade mais próxima: {list_date_val[0]}\n'
+            f'Data de validade mais próxima: {expiration_date}\n'
             f'Empresa com maior quantidade de produtos estocados:'
             f' {most_products}')
